0c99ebd633335f2ec6859bd52286496453d18f64
Christian Bouwense <bouwensec@gmail.com>
2020-04-22 14:42:54 -0400

    finessed the posts

--- ui/src/profile/components/Post.tsx
+++ ui/src/profile/components/Post.tsx
@@ -3,16 +3,29 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(() => ({
   avatar: {
     "width": "100px",
     "height": "100px"
   },
-  post: {
+  root: {
     "box-shadow": "10px 10px 20px #c9c9c9",
     "padding": "15px",
     "width": "100%"
+  },
+  postHeader: {
+    "display": "flex"
+  },
+  postHeaderDetails: {
+    "display": "flex",
+    "flex-direction": "column",
+    "align-items": "flex-start",
+    "margin-left": "15px"
+  },
+  postText: {
+    "margin-top": "15px"
   }
 }));
 
@@ -28,11 +41,15 @@ export const Post = (props: PostProps) => {
   const classes = useStyles();
   
   return (
-    <Card className={classes.post}>
-      <Avatar className={classes.avatar} src={props.avatarSrc} />
-      <p>{props.name}</p>
-      <p>{props.date}</p>
-      <p>{props.text}</p>
+    <Card className={classes.root}>
+      <div className={classes.postHeader}>
+        <Avatar className={classes.avatar} src={props.avatarSrc} />
+        <div className={classes.postHeaderDetails}>
+          <Typography variant="h4">{props.name}</Typography>
+          <Typography variant="h6">{props.date}</Typography>
+        </div>
+      </div>
+      <Typography className={classes.postText} variant="body1">{props.text}</Typography>
     </Card>
   );
 }
--- ui/src/profile/components/Posts.tsx
+++ ui/src/profile/components/Posts.tsx
@@ -26,7 +26,7 @@ export const Posts = () => {
             avatarSrc="https://66.media.tumblr.com/9c0f217919b12c3b378ba878a4e370b1/tumblr_o7sgjyR0fP1s7x9qwo2_500.jpg"
             date="2020-01-01 01:01:01"
             name="Gil Galad"
-            text="This is some post text"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           />
         </Grid>
         <Grid item xs={12}>
@@ -34,7 +34,7 @@ export const Posts = () => {
             avatarSrc="https://66.media.tumblr.com/9c0f217919b12c3b378ba878a4e370b1/tumblr_o7sgjyR0fP1s7x9qwo2_500.jpg"
             date="2020-01-01 01:01:01"
             name="Gil Galad"
-            text="This is some post text"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           />
         </Grid>
         <Grid item xs={12}>
@@ -42,7 +42,15 @@ export const Posts = () => {
             avatarSrc="https://66.media.tumblr.com/9c0f217919b12c3b378ba878a4e370b1/tumblr_o7sgjyR0fP1s7x9qwo2_500.jpg"
             date="2020-01-01 01:01:01"
             name="Gil Galad"
-            text="This is some post text"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Post
+            avatarSrc="https://66.media.tumblr.com/9c0f217919b12c3b378ba878a4e370b1/tumblr_o7sgjyR0fP1s7x9qwo2_500.jpg"
+            date="2020-01-01 01:01:01"
+            name="Gil Galad"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           />
         </Grid>
       </Grid>
